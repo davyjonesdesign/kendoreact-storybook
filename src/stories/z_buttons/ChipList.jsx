d9ebfcf17482ac9ef3d98113d0a8f0ab0fb18d99
Davy Jones <dmjones.me@gmail.com>
2025-07-22 16:17:28 -0500

    ChipList

--- src/stories/z_buttons/ChipList.jsx
+++ src/stories/z_buttons/ChipList.jsx
@@ -1,37 +1,61 @@
+// ChipList.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ChipList as KendoChipList } from "@progress/kendo-react-buttons";
+import { ChipList as KendoChipList, Chip } from "@progress/kendo-react-buttons";
 import '../assets/index.scss';
 
-export const ChipList = ({...props }) => {
+/**
+ * ChipList component wraps Kendo UI's ChipList with customizable styling and behavior.
+ * 
+ * @param {Object} props - Component props
+ * @param {string} fillMode - Visual style: 'solid' or 'outline' 
+ * @param {boolean} removable - Whether chips can be removed with X button
+ * @param {string} selection - Selection mode: 'single', 'multiple', or 'none'
+ * @param {Array} data - Array of chip data objects with text and value
+ * @param {Function} onChange - Callback when selection changes
+ * @param {Function} onRemove - Callback when chip is removed
+ */
+export const ChipList = ({ fillMode = 'solid', customChip, removable, ...props }) => {
   const fruits = [
-  {
-    text: "Berry",
-    value: "berry",
-  },
-  {
-    text: "Apple",
-    value: "apple",
-  },
-  {
-    text: "Strawberry",
-    value: "strawberry",
-  },
-  {
-    text: "Banana",
-    value: "banana",
-  },
-  {
-    text: "Kiwi",
-    value: "kiwi",
-  },
-];
+    {
+      text: "Berry",
+      value: "berry",
+    },
+    {
+      text: "Apple",
+      value: "apple",
+    },
+    {
+      text: "Strawberry",
+      value: "strawberry",
+    },
+    {
+      text: "Banana",
+      value: "banana",
+    },
+    {
+      text: "Kiwi",
+      value: "kiwi",
+    },
+  ];
+
+  // Custom chip component with fillMode and removable control
+  const ChipWithFillMode = (chipProps) => {
+    return (
+      <Chip 
+        {...chipProps} 
+        fillMode={fillMode}
+        removable={removable}
+      />
+    );
+  };
+
   return (
-    <>
-    <KendoChipList {...props}
+    <KendoChipList 
+      {...props}
       defaultData={fruits}
+      chip={customChip || ChipWithFillMode}
     />
-    </>
   );
 };
 
@@ -45,20 +69,25 @@ ChipList.propTypes = {
   defaultValue: PropTypes.any,
   dir: PropTypes.oneOf(['ltr', 'rtl', 'auto']),
   disabled: PropTypes.bool,
+  fillMode: PropTypes.oneOf(['solid', 'outline']), // Add fillMode prop
   id: PropTypes.string,
   name: PropTypes.string,
+  onChange: PropTypes.func,
+  onRemove: PropTypes.func,
+  removable: PropTypes.bool,
   required: PropTypes.bool,
   selection: PropTypes.oneOf(['single', 'multiple', 'none']),
-  size: PropTypes.oneOf(['small', 'medium', 'large', 'null']),
   tabIndex: PropTypes.number,
   textField: PropTypes.string,
   valid: PropTypes.bool,
   validationMessage: PropTypes.string,
   validityStyles: PropTypes.bool,
   value: PropTypes.any,
-  valueField: PropTypes.string
+  valueField: PropTypes.string,
+  customChip: PropTypes.func
 };
 
 ChipList.defaultProps = {
-  selection: 'multiple'
+  selection: 'none',
+  fillMode: 'solid'
 };
--- src/stories/z_buttons/ChipList.stories.jsx
+++ src/stories/z_buttons/ChipList.stories.jsx
@@ -1,18 +1,74 @@
 import React from 'react';
-
 import { ChipList } from './ChipList';
 
-export default {
+
+const meta = {
   title: 'Buttons/ChipList',
   component: ChipList,
+  tags: ['autodocs'], // Enables auto-generated Docs tab
+  parameters: {
+    docs: {
+      description: {
+        component: 'A flexible chip list component built on top of Kendo UI ChipList. Supports different visual styles, selection modes, and removable chips.',
+      },
+    },
+  },
   argTypes: {
-
+    fillMode: {
+      control: {
+        type: 'radio',
+        options: ['solid', 'outline']
+      },
+      description: 'Controls the visual style of all chips'
+    },
+    selection: {
+      control: {
+        type: 'select',
+        options: ['single', 'multiple', 'none'],
+        defaultValue: 'none'
+      },
+      description: 'Selection behavior for chips'
+    },
+    removable: {
+      control: 'boolean',
+      defaultValue: false,
+      description: 'Whether chips show remove (X) buttons'
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disables all chips in the list'
+    },
+    data: {
+      control: 'object',
+      description: 'Array of chip data objects with text and value properties'
+    }
   },
 };
 
-const Template = (args) => <ChipList {...args} />;
+export default meta;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Solid = {
+  args: {
+    fillMode: 'solid'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Default solid style with multiple selection enabled.',
+      },
+    },
+  }
 };
-Primary.storyName = 'Chip List';
+
+export const Outline = {
+  args: {
+    fillMode: 'outline'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Outline style showing chip borders without filled backgrounds.',
+      },
+    },
+  }
+};
